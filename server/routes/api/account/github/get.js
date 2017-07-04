a5aa39d16e9850580856cb21a6804eb4a0f0e8b7
tmarshall <timothyjmarshall@gmail.com>
2017-07-04 09:46:37 +0700

    account.github getter

---
+++ server/routes/api/account/github/get.js
@@ -0,0 +1,38 @@
+const Route = require('conjure-core/classes/Route');
+const UnexpectedError = require('conjure-core/modules/err').UnexpectedError;
+
+const route = new Route({
+  requireAuthentication: true
+});
+
+/*
+  Repos listing
+ */
+route.push((req, res, next) => {
+  const DatabaseTable = require('conjure-core/classes/DatabaseTable');
+  const accountGithub = new DatabaseTable('account_github');
+
+  accountGithub.select({
+    account: req.user.id
+  }, (err, rows) => {
+    if (err) {
+      return next(err);
+    }
+
+    // should not be possible
+    if (!rows.length) {
+      return next(new UnexpectedError('Could not find GitHub account record'));
+    }
+
+    // should not be possible
+    if (rows.length > 1) {
+      return next(new UnexpectedError('Expected a signle row for GitHub account record, received multiple'));
+    }
+
+    res.send({
+      account: rows[0]
+    });
+  });
+});
+
+module.exports = route;
