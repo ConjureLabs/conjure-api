c6ed416d0b13f99e8fed9059182ab785ff02af21
tmarshall <timothyjmarshall@gmail.com>
2017-07-15 18:26:07 -0700

    skip onboard POST endpoint

---
+++ server/routes/api/onboard/skip/post.js
@@ -0,0 +1,43 @@
+const Route = require('conjure-core/classes/Route');
+const NotFoundError = require('conjure-core/modules/err').NotFoundError;
+const UnexpectedError = require('conjure-core/modules/err').UnexpectedError;
+
+const route = new Route({
+  requireAuthentication: true
+});
+
+route.push((req, res, next) => {
+  const DatabaseTable = require('conjure-core/classes/DatabaseTable');
+  const account = new DatabaseTable('account');
+
+  account.select({
+    id: req.user.id
+  }, (err, rows) => {
+    if (err) {
+      return next(err);
+    }
+
+    // record does not exist in our db - should not happen
+    if (!rows.length) {
+      return next(new NotFoundError('Account not found'));
+    }
+
+    // checking if user should be able to do this
+    if (rows[0].onboarded === true) {
+      return next(new UnexpectedError('Account already onboarded'));
+    }
+
+    account.onboarded = true;
+    account.updated = new Date();
+
+    account.save(err => {
+      if (err) {
+        return next(err);
+      }
+
+      res.send({});
+    });
+  });
+});
+
+module.exports = route;
