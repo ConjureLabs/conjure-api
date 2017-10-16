20ee103b5bedd10710c16a91a70c975c48994116
tmarshall <timothyjmarshall@gmail.com>
2017-10-15 17:39:42 -0700

    more v8 await logic changed

--- server/routes/api/account/card/$cardStripeId/delete.js
+++ server/routes/api/account/card/$cardStripeId/delete.js
@@ -1,6 +1,5 @@
 const Route = require('conjure-core/classes/Route');
-const UnexpectedError = require('conjure-core/modules/err').UnexpectedError;
-const ContentError = require('conjure-core/modules/err').ContentError;
+const { UnexpectedError, ContentError } = require('conjure-core/modules/err');
 
 const route = new Route({
   requireAuthentication: true
@@ -12,119 +11,74 @@ const route = new Route({
 route.push((req, res, next) => {
   const stripeCardId = req.params.cardStripeId;
 
-  // pulling various record in parallel, to improve speed
-  const parallel = {};
+  const stripeCustomer = pullStripeCustomerInstance(req, callback);
 
-  // pull stripe customer instance, based on account record
-  parallel.stripeCustomer = callback => {
-    pullStripeCustomerInstance(req, callback);
-  };
+  const DatabaseTable = require('conjure-core/classes/DatabaseTable');
+  const accountCard = new DatabaseTable('account_card');
 
-  // pull account card rows
-  parallel.accountCards = callback => {
-    const DatabaseTable = require('conjure-core/classes/DatabaseTable');
-    const accountCard = new DatabaseTable('account_card');
+  if (!req.user) {
+    throw new UnexpectedError('No req.user available');
+  }
 
-    if (!req.user) {
-      return next(new UnexpectedError('No req.user available'));
-    }
-
-    accountCard.select({
-      account: req.user.id
-    }, (err, rows) => {
-      callback(err, rows);
-    });
-  };
-
-  const async = require('async');
-  async.parallel(parallel, (err, records) => {
-    if (err) {
-      return next(err);
-    }
-
-    // verify user even has this card
-    const cardMatches = records.accountCards.filter(card => {
-      return card.stripe_id === stripeCardId;
-    });
-
-    if (!cardMatches.length) {
-      return next(new ContentError('This account does not have the corresponding card on record'));
-    } else if (cardMatches.length > 1) {
-      // this should not happen
-      // todo: maybe allow this and clear _all_ of these records at once?
-      return next(new ContentError('There are more than one cards on file with the same id'));
-    }
-
-    const cardMatch = cardMatches[0];
-    const series = [];
-
-    // first delete our own reference to it
-    series.push(callback => {
-      cardMatch.delete(callback);
-    });
-
-    // now delete the stripe id
-    // todo: have a worker routinely clean up stipe cards that have no association in our db, in case our delete works and this call does not?
-    series.push(callback => {
-      const Card = require('conjure-core/classes/Stripe/Card');
-      Card.delete(records.stripeCustomer, stripeCardId, callback);
-    });
-
-    async.series(series, err => {
-      if (err) {
-        return next(err);
-      }
-
-      res.send({});
-    });
+  const accountCards = await accountCard.select({
+    account: req.user.id
   });
+
+  // verify user even has this card
+  const cardMatches = accountCards.filter(card => {
+    return card.stripe_id === stripeCardId;
+  });
+
+  if (!cardMatches.length) {
+    throw new ContentError('This account does not have the corresponding card on record');
+  } else if (cardMatches.length > 1) {
+    // this should not happen
+    // todo: maybe allow this and clear _all_ of these records at once?
+    throw new ContentError('There are more than one cards on file with the same id');
+  }
+
+  const cardMatch = cardMatches[0];
+  const series = [];
+
+  // first delete our own reference to it
+  await cardMatch.delete();
+
+  // now delete the stripe id
+  // todo: have a worker routinely clean up stipe cards that have no association in our db, in case our delete works and this call does not?
+  const Card = require('conjure-core/classes/Stripe/Card');
+  await Card.delete(await stripeCustomer, stripeCardId);
+
+  res.send({});
 });
 
 module.exports = route;
 
-function pullStripeCustomerInstance(req, callback) {
-  const waterfall = [];
+async function pullStripeCustomerInstance(req) {
+  const DatabaseTable = require('conjure-core/classes/DatabaseTable');
+  const account = new DatabaseTable('account');
 
-  // getting full user account record
-  waterfall.push(callback => {
-    const DatabaseTable = require('conjure-core/classes/DatabaseTable');
-    const account = new DatabaseTable('account');
-
-    account.select({
-      id: req.user.id
-    }, (err, rows) => {
-      if (err) {
-        return callback(err);
-      }
-
-      // should not be possible
-      if (!rows.length) {
-        return callback(new UnexpectedError('Could not find account record'));
-      }
-
-      // should not be possible
-      if (rows.length > 1) {
-        return callback(new UnexpectedError('Expected a single row for account record, received multiple'));
-      }
-
-      callback(null, rows[0]);
-    });
+  const accountRows = await account.select({
+    id: req.user.id
   });
 
-  // getting/creating stripe customer record
-  waterfall.push((account, callback) => {
-    // if no account stripe_id, then error, since we expect it
-    if (typeof account.stripe_id !== 'string' || !account.stripe_id) {
-      return callback(new ContentError('Account is not associated to any Stripe records'));
-    }
+  // should not be possible
+  if (!rows.length) {
+    throw new UnexpectedError('Could not find account record');
+  }
 
-    const Customer = require('conjure-core/classes/Stripe/Customer');
+  // should not be possible
+  if (rows.length > 1) {
+    throw new UnexpectedError('Expected a single row for account record, received multiple');
+  }
 
-    Customer.retrieve(req.user.id, account.stripe_id, (err, customerRecord) => {
-      callback(err, customerRecord);
-    });
-  });
+  const account = accountRows[0];
 
-  const asyncWaterfall = require('conjure-core/modules/async/waterfall');
-  asyncWaterfall(waterfall, callback);
+  // if no account stripe_id, then error, since we expect it
+  if (typeof account.stripe_id !== 'string' || !account.stripe_id) {
+    throw new ContentError('Account is not associated to any Stripe records');
+  }
+
+  const Customer = require('conjure-core/classes/Stripe/Customer');
+
+  return await Customer.retrieve(req.user.id, account.stripe_id);
 }
--- server/routes/api/account/card/post.js
+++ server/routes/api/account/card/post.js
@@ -1,5 +1,5 @@
 const Route = require('conjure-core/classes/Route');
-const UnexpectedError = require('conjure-core/modules/err').UnexpectedError;
+const { UnexpectedError } = require('conjure-core/modules/err');
 const log = require('conjure-core/modules/log')('account card entry');
 
 const route = new Route({
@@ -23,107 +23,70 @@ route.push((req, res, next) => {
   const DatabaseTable = require('conjure-core/classes/DatabaseTable');
   const cardData = req.body.card;
   const addressData = req.body.address;
-  const waterfall = [];
 
   // getting full user account record
-  waterfall.push(callback => {
-    const account = new DatabaseTable('account');
+  const account = new DatabaseTable('account');
 
-    account.select({
-      id: req.user.id
-    }, (err, rows) => {
-      if (err) {
-        return callback(err);
-      }
-
-      // should not be possible
-      if (!rows.length) {
-        return callback(new UnexpectedError('Could not find account record'));
-      }
-
-      // should not be possible
-      if (rows.length > 1) {
-        return callback(new UnexpectedError('Expected a single row for account record, received multiple'));
-      }
-
-      callback(null, rows[0]);
-    });
+  const accountRows = await account.select({
+    id: req.user.id
   });
 
+  // should not be possible
+  if (!accountRows.length) {
+    throw new UnexpectedError('Could not find account record');
+  }
+
+  // should not be possible
+  if (accountRows.length > 1) {
+    throw new UnexpectedError('Expected a single row for account record, received multiple');
+  }
+
+  const account = accountRows[0];
+
   // getting/creating stripe customer record
-  waterfall.push((account, callback) => {
-    const Customer = require('conjure-core/classes/Stripe/Customer');
+  const Customer = require('conjure-core/classes/Stripe/Customer');
 
-    // if account has a stripe customer record, get it and continue
-    if (account.stripe_id) {
-      Customer.retrieve(req.user.id, account.stripe_id, (err, customerRecord) => {
-        callback(err, account, customerRecord);
-      });
-      return;
-    }
+  // if account has a stripe customer record, get it and continue
+  const customer = account.stripe_id ? await Customer.retrieve(req.user.id, account.stripe_id) : await new Customer(req.user.id, {
+    email: account.email,
+    name: account.name
+  }).save();
 
-    // if existing customer record does not exist, then we have to create one
-    new Customer(req.user.id, {
-      email: account.email,
-      name: account.name
-    }).save((err, customerRecord) => {
-      if (err) {
-        return callback(err);
-      }
-
-      // store id for stripe customer record, on account row
-      account.stripe_id = customerRecord.id;
-      account.updated = new Date();
-      account.save(err => {
-        callback(err, account, customerRecord);
-      });
-    });
+  // store id for stripe customer record, on account row
+  account.stripe_id = customer.id;
+  account.updated = new Date();
+  account.save(err => {
+    callback(err, account, customer);
   });
 
   // add credit card
-  waterfall.push((account, customer, callback) => {
-    const Card = require('conjure-core/classes/Stripe/Card');
+  const Card = require('conjure-core/classes/Stripe/Card');
 
-    new Card(customer, {
-      cvc: cardData.cvc,
-      name: cardData.name,
-      number: cardData.number,
-      expiration: {
-        month: cardData.mm,
-        year: cardData.yyyy
-      },
-      address: {
-        line1: addressData.addr1,
-        line2: addressData.addr2,
-        city: addressData.city,
-        state: addressData.state,
-        zip: addressData.zip,
-        country: addressData.country
-      }
-    }, req.body).save((err, cardRecord) => {
-      if (err) {
-        return callback(err);
-      }
-
-      DatabaseTable.insert('account_card', {
-        account: account.id,
-        stripe_id: cardRecord.id,
-        added: new Date()
-      }, err => {
-        callback(err);
-      });
-    });
-  });
-
-  const asyncWaterfall = require('conjure-core/modules/async/waterfall');
-  asyncWaterfall(waterfall, err => {
-    if (err) {
-      return next(err);
+  const creditCard = await new Card(customer, {
+    cvc: cardData.cvc,
+    name: cardData.name,
+    number: cardData.number,
+    expiration: {
+      month: cardData.mm,
+      year: cardData.yyyy
+    },
+    address: {
+      line1: addressData.addr1,
+      line2: addressData.addr2,
+      city: addressData.city,
+      state: addressData.state,
+      zip: addressData.zip,
+      country: addressData.country
     }
+  }, req.body).save();
 
-    // all good
-    res.send({});
+  await DatabaseTable.insert('account_card', {
+    account: account.id,
+    stripe_id: cardRecord.id,
+    added: new Date()
   });
+
+  res.send({});
 });
 
 module.exports = route;
