c053f5bd5f84da5b162753b5bfd838971c1dfb93
tmarshall <timothyjmarshall@gmail.com>
2017-11-19 15:15:43 -0800

    onboard plan selection endpoint

---
+++ server/routes/api/onboard/plan/selection/post.js
@@ -0,0 +1,21 @@
+const Route = require('conjure-core/classes/Route');
+const { ContentError } = require('conjure-core/modules/err');
+
+const route = new Route({
+  requireAuthentication: true
+});
+
+route.push(async (req, res) => {
+  if (!req.body.label || !req.body.value) {
+    throw new ContentError('Payload missing or in an unexpected format');
+  }
+
+  res.cookie('conjure-onboard-plan', req.body, {
+    maxAge: 259200000, // 3 days
+    httpOnly: true
+  });
+
+  return res.send({});
+});
+
+module.exports = route;
