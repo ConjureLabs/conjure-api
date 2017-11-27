ea65c3a74bf79f2df476caa026240a72d8ddbb0b
tmarshall <timothyjmarshall@gmail.com>
2017-11-26 17:36:29 -0800

    user repos, single org repos

---
+++ server/routes/api/account/repos/get.js
@@ -0,0 +1,57 @@
+const Route = require('conjure-core/classes/Route');
+
+const route = new Route({
+  requireAuthentication: true
+});
+
+/*
+  Repos listing
+ */
+route.push(async (req, res) => {
+  const UniqueArray = require('conjure-core/classes/Array/UniqueArray');
+  const GitHubRepo = require('conjure-core/classes/Repo/GitHub');
+
+  // todo: assumes account has a github record in our db - we should have more handlers for services like bitbucket
+  const apiGetAccountGitHub = require('../account/github/get.js').call;
+  const gitHubAccount = (await apiGetAccountGitHub(req)).account;
+
+  const github = require('octonode');
+  const gitHubClient = github.client(gitHubAccount.access_token);
+
+  // just for debub purposes
+  // todo: move or remove this
+  gitHubClient.limit((err, left, max, reset) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('left', left);
+      console.log('max', max);
+      console.log('reset', reset);
+    }
+  });
+
+  const repos = await promisifyGitHubUserRepos(gitHubClient, gitHubAccount);
+
+  const sortInsensitive = require('conjure-core/modules/utils/Array/sort-insensitive');
+  sortInsensitive(repos, 'fullName');
+
+  // todo: stop sending by org all the time - it's an overhead most of the time
+  return res.send({
+    [gitHubAccount.username]: repos
+  });
+});
+
+function promisifyGitHubUserRepos(client, gitHubAccount) {
+  return new Promise((resolve, reject) => {
+    client
+      .user(gitHubAccount.username)
+      .repos((err, repos) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(repos);
+      });
+  });
+}
+
+module.exports = route;
---
+++ server/routes/api/org/$orgName/repos/get.js
@@ -0,0 +1,59 @@
+const Route = require('conjure-core/classes/Route');
+
+const route = new Route({
+  requireAuthentication: true
+});
+
+/*
+  Repos listing
+ */
+route.push(async (req, res) => {
+  const orgName = req.params.orgName;
+
+  const UniqueArray = require('conjure-core/classes/Array/UniqueArray');
+  const GitHubRepo = require('conjure-core/classes/Repo/GitHub');
+
+  // todo: assumes account has a github record in our db - we should have more handlers for services like bitbucket
+  const apiGetAccountGitHub = require('../account/github/get.js').call;
+  const githubAccount = (await apiGetAccountGitHub(req)).account;
+
+  const github = require('octonode');
+  const githubClient = github.client(githubAccount.access_token);
+
+  // just for debub purposes
+  // todo: move or remove this
+  githubClient.limit((err, left, max, reset) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('left', left);
+      console.log('max', max);
+      console.log('reset', reset);
+    }
+  });
+
+  const repos = await promisifyGitHubOrgRepos(githubClient, orgName);
+
+  const sortInsensitive = require('conjure-core/modules/utils/Array/sort-insensitive');
+  sortInsensitive(repos, 'fullName');
+
+  // todo: stop sending by org all the time - it's an overhead most of the time
+  return res.send({
+    [orgName]: repos
+  });
+});
+
+function promisifyGitHubOrgRepos(client, orgName) {
+  return new Promise((resolve, reject) => {
+    client
+      .org(orgName)
+      .repos((err, repos) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(repos);
+      });
+  });
+}
+
+module.exports = route;
