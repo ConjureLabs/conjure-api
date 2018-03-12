659badb442fd327ea1acca0cf931ed16571845c7
tmarshall <timothyjmarshall@gmail.com>
2018-03-11 19:31:06 -0700

    passing back billing attributes needed

--- server/routes/api/org/$orgName/billing/plan/get.js
+++ server/routes/api/org/$orgName/billing/plan/get.js
@@ -1,4 +1,5 @@
 const Route = require('@conjurelabs/route')
+const { NotFoundError } = require('@conjurelabs/err')
 
 const route = new Route({
   requireAuthentication: true
@@ -6,18 +7,35 @@ const route = new Route({
 
 route.push(async (req, res) => {
   const { query } = require('@conjurelabs/db')
+  const DatabaseTable = require('@conjurelabs/db/table')
 
   // must get github org id, based on name
   const apiGetGitHubOrgInfo = require('../../get.js').call
   const githubOrg = await apiGetGitHubOrgInfo(req)
 
   // unset any existing plans for the org
-  const result = await query(`
+  const orgPlanResult = await query(`
     SELECT * FROM github_org_monthly_billing_plan
     WHERE deactivated IS NULL
     AND activated IS NOT NULL
     AND org_id = $1
   `, [githubOrg.id])
+  const orgPlan = result.rows[0]
 
-  return res.send(result.rows[0])
+  if (!orgPlan) {
+    throw new NotFoundError('No billing plan found for org')
+  }
+
+  // getting plan info
+  const matchingPlans = await DatabaseTable.select('monthlyBillingPlan', {
+    id: orgPlan.monthlyBillingPlan
+  })
+
+  if (!matchingPlans.length) {
+    throw new NotFoundError('No billing plan found')
+  }
+
+  return res.send(matchingPlans[0])
 })
+
+module.exports = route
