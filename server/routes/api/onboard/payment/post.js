6752aaddebc15c71960ab71986a957d0a357285b
tmarshall <timothyjmarshall@gmail.com>
2018-05-06 10:39:36 -0700

    fix email wording & add logo

--- server/routes/api/onboard/payment/post.js
+++ server/routes/api/onboard/payment/post.js
@@ -24,10 +24,11 @@ async function emailUser(req) {
     html: `
       <body>
         <div style="display: block; padding: 20px; text-align: center;">
+          <img src="${config.app.web.url}/static/images/logos/conjure/full-color.svg" style="height: 32px; width:176px; margin-bottom: 12px;" />
           <h2 style="display: block; font-size: 32px; font-weight: 600; margin-bottom: 42px; color: #434245;">You've joined Conjure 👏</h2>
           <p style="display: block; font-size: 16px; font-weight: 100; margin-bottom: 32px; color: #434245;">At any time you can manage what Conjure watches at <a href='${config.app.web.url}'>${config.app.web.host}</a>.</p>
           <p style="display: block; font-size: 14px; font-weight: 100; margin-bottom: 24px; color: #434245;">Don't forget to read <a href='${config.app.web.url}/docs/configuration'>our configuration docs</a> to make sure your repos are set up properly.</p>
-          <p style="display: block; font-size: 14px; font-weight: 100; margin-bottom: 10px; color: #434245;">We're happy you're you've joined us!</p>
+          <p style="display: block; font-size: 14px; font-weight: 100; margin-bottom: 10px; color: #434245;">We're happy you've joined us!</p>
         </div>
       </body>
     `
