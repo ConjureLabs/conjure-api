3aca846d22b6e2cc3e7a973aeeb4e22652ee31f3
tmarshall <timothyjmarshall@gmail.com>
2018-05-28 14:43:58 -0700

    bump route

--- server/index.js
+++ server/index.js
@@ -331,7 +331,7 @@ function slackNotifySignup(account) {
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true)
   res.header('Access-Control-Allow-Origin', req.headers.origin)
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE')
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept')
   next()
 })
